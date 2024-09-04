e26c314cf7458a1a44944c036b0d29e16196972c
Karnav <karnavpopat@gmail.com>
2024-09-04 16:23:52 +0530

    feat: urban local body budgets

---
+++ ULB/ulb_budgets.py
@@ -0,0 +1,92 @@
+import os
+import requests
+import re
+import warnings
+import pandas as pd
+from pypdf import PdfReader
+from pprint import pprint
+from tqdm import tqdm
+import random
+import json
+
+tqdm.pandas()
+warnings.filterwarnings("ignore")
+
+
+def parse_one_pdf(file_name, read_pdf):
+
+    budget = {}
+
+    for index, page in enumerate(read_pdf.pages):
+
+        text = page.extract_text().replace("\n", "")
+        if "PARA: 4 FINANCIAL POSITION" in text:
+
+            if "GRANDTOTAL" in text or "GRAND TOTAL" in text:
+                text = text.replace("GRAND TOTAL", "GRANDTOTAL").replace("Details of Closing Balance and Comments", "Comments")
+            else:
+                text = read_pdf.pages[index + 1].extract_text().replace("\n", "").replace("GRAND TOTAL", "GRANDTOTAL").replace("Details of Closing Balance and Comments", "Comments")
+                index += 1
+
+            try:
+                budget_string = text.split("GRANDTOTAL ")[1].split("Comments")[0]
+
+                keys = ["Opening_Balance", "Receipt", "Expenditure", "Closing_Balance", "Check"]
+                values = [float(x) for i, x in enumerate(re.findall(r'\d*\.\d{2}', budget_string)) if i in [0, 1, 3, 4, 5]]
+
+                budget = dict(zip(keys, values))
+                budget["PDF Number"] = file_name
+                budget["Page text"] = text
+
+            except:
+                print(f"Failed to extract from Page {index} of {file_name}")
+
+            break
+
+    return budget
+
+
+def parse_one_municipality(path):
+
+    municipality_budgets = []
+
+    for file_name in tqdm(os.listdir(path)):
+
+        reader = PdfReader(path + file_name)
+        municipality_budgets.append(parse_one_pdf(file_name, reader))
+
+    df = pd.DataFrame(municipality_budgets)
+    pdf_column = df.pop("PDF Number")
+    df.insert(0, "PDF Number", pdf_column.apply(lambda x: x.split(".pdf")[0]))
+
+    return df
+
+
+def check_against_manual(df):
+
+    manual = pd.read_csv("D:/assorted/Dropbox/ULB_Data_Odisha/Urban/Budget_Coded/BMC_Budget_Ankit.csv").dropna(subset=["PDF Number"])
+    merged = pd.merge(df, manual, how="left", on="PDF Number", suffixes=("_s", "_m"))
+    merged.to_csv("./ulb_budgets_merged_methods.csv", index=False)
+
+    match = pd.DataFrame()
+    match["PDF Number"] = merged["PDF Number"]
+    match["OB_match"] = merged["Opening_Balance_s"] - merged["Opening_Balance_m"]
+    match["Receipt_match"] = merged["Receipt_s"] - merged["Receipt_m"]
+    match["Exp_match"] = merged["Expenditure_s"] - merged["Expenditure_m"]
+    match["CB_match"] = merged["Closing_Balance_s"] - merged["Closing_Balance_m"]
+    match["Check_match"] = merged["Check_s"] - merged["Check_m"]
+    match["CB_Check_s_match"] = merged["Closing_Balance_s"] - merged["Check_s"]
+    match["CB_Check_m_match"] = merged["Closing_Balance_m"] - merged["Check_m"]
+    match.iloc[:, 1:] = match.iloc[:, 1:].map(lambda x: float(x) if (float(x) > 5 or float(x) < -5) else False)
+    match.to_csv("./ulb_budgets_matched_methods.csv", index=False)
+
+
+if __name__ == "__main__":
+
+    municipality_name = "Dhenkanal"
+    dir_path = f"D:/assorted/Dropbox/ULB_Data_Odisha/Municipality/{municipality_name}/"
+
+    data = parse_one_municipality(dir_path)
+    data.to_csv("./ulb_budgets_parsed.csv", index=False)
+
+    check_against_manual(data)
